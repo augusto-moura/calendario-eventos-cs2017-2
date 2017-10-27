082b79608479513570a9f5eb7cb3f17018bdd1ac
augusto-moura <augusto.borgesm@gmail.com>
2017-10-27 17:48:33 -0200

    Estruturação básica spring boot

---
+++ src/test/java/ufg/inf/es/calendario/CalendarioApplicationTests.java
@@ -0,0 +1,16 @@
+package ufg.inf.es.calendario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalendarioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

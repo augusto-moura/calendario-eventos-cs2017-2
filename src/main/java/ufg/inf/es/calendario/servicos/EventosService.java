738447f27088190518f27383c3693ce92c9c5b63
Breno <breno@debian.Home>
2017-11-13 17:56:00 -0200

    Documentação da classe EventosService.java

--- src/main/java/ufg/inf/es/calendario/servicos/EventosService.java
+++ src/main/java/ufg/inf/es/calendario/servicos/EventosService.java
@@ -9,13 +9,31 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.ArrayList;
 import java.util.List;
+/*
+ * Aluno: Breno de Melo Gomes e Augusto Borges de moura
+ * Versão: <1.0>
+ * Data de Criação: 13/11/2017
+ * Copyright (c) 2017 UFG - www.ufg.br
+ * Todos os direitos reservados.
+ *
+ * Esta classe tem o propósito de adicionar eventos a estrutura 
+ * calendário.
+ */
 
 @Named
 public class EventosService {
-
+    
     @Inject
     private EventosDao eventosDao;
 
+    /**
+    * Consulta todos os eventos de um usuário.
+    *
+    * @param null - nenhum paramentro
+    * @return lista - arryList contendo todos os eventos de um usuário cadastrado no
+    * sistema.
+    * @throws IOException - no caso de problema  a função não retorna nenhuma exceção.
+    */
     public List<Evento> consultarTodos() {
         List<Evento> lista = new ArrayList<>();
 
@@ -26,6 +44,14 @@ public class EventosService {
         return lista;
     }
 
+    /**
+    * Gera relatório das notas do aluno.
+    *
+    * @param Evento - objeto com as suas eventos.
+    * @return objeto do tipo eventoDao - objeto contendo todos os eventos de cadastrados. 
+    * no sistema.
+    * @throws IOException - no caso de problema  a função não retorna nenhuma exceção.
+    */
     public Evento salvar(Evento evento) {
         return eventosDao.save(evento);
     }

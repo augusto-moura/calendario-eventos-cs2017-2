1ea01a5630324648a5ea1eae0008bd2638f9c0ae
Breno <breno@debian.Home>
2017-11-13 18:13:32 -0200

    documentação da classe EventosResource.java

--- src/main/java/ufg/inf/es/calendario/recursos/EventosResource.java
+++ src/main/java/ufg/inf/es/calendario/recursos/EventosResource.java
@@ -13,6 +13,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List; 
+/**
+ * Aluno: Breno de Melo Gomes e Augusto Borges de moura
+ * Versão: <1.0>
+ * Data de Criação: 13/11/2017
+ * Copyright (c) 2017 UFG - www.ufg.br
+ * Todos os direitos reservados.
+ *
+ * Esta classe tem o propósito de ser a interface de comunicação 
+ * do o servidor e a aplicação calendário.
+ */
 
 @Named
 @Path("/eventos")
@@ -22,11 +32,27 @@ public class EventosResource {
     @Inject
     private EventosService eventosService;
 
+    /**
+    *  Consulta todos os eventos de um usuário.
+    *
+    * @param null - nenhum paramentro
+    * @return lista - arryList contendo todos os eventos de um usuário cadastrado no
+    * sistema.
+    * @throws IOException - no caso de problema  a função não retorna nenhuma exceção.
+    */
     @GET
     public List<Evento> consultaTodos() {
         return eventosService.consultarTodos();
     }
-
+    
+    /**
+    * Adiciona um novo evento ao calendário de um usuário
+    *
+    * @param Evento - objeto com as suas eventos.
+    * @return objeto do tipo evento - objeto contendo todos os eventos de cadastrados. 
+    * no sistema.
+    * @throws IOException - no caso de problema  a função não retorna nenhuma exceção.
+    */
     @POST
     public Evento criarNovo(final Evento evento) {
         return eventosService.salvar(evento);
